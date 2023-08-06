9d2ef8e5f07afa2d9d80eebc2e3c6e7d1c34b8b7
Bakr Hawsawi <bakrhawsawi4@gmail.com>
2023-08-06 14:33:09 +0300

    add HW 11

---
+++ assignment---11/src/main/java/com/example/assignment11/Controller.java
@@ -0,0 +1,35 @@
+package com.example.assignment11;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(path="/name")
+    public String name(){
+        return "My name is Bakr";
+    }
+
+    @GetMapping(path="/age")
+    public String age(){
+        return "My age is 23";
+    }
+
+    @GetMapping(path="/check/status")
+    public String check(){
+        return "Everything OK";
+    }
+
+    @GetMapping(path="/health")
+    public String health(){
+        return "Server health is up and running";
+    }
+
+    @GetMapping(path="/names")
+    public String[] names(){
+        return new String[]{"Bakr","Salem","Baraa"};
+    }
+
+
+}
